be72bd03c82b518eeda7b0ee0a7cd16fcb9fb5bf
ebaze-tech <139893207+ebaze-tech@users.noreply.github.com>
2024-07-05 23:56:57 +0100

    Update organisationRoutes.js

--- routes/organisationRoutes.js
+++ routes/organisationRoutes.js
@@ -9,3 +9,22 @@ router.get('/', getOrganisations);
 router.post('/', createOrganisation);
 
 module.exports = router;
+
+
+
+
+
+
+
+
+const express = require('express');
+const router = express.Router();
+const organisationController = require('../controllers/organisationController');
+const authMiddleware = require('../middlewares/authMiddleware');
+
+router.get('/', authMiddleware, organisationController.getAllOrganisations);
+router.get('/:orgId', authMiddleware, organisationController.getOrganisation);
+router.post('/', authMiddleware, organisationController.createOrganisation);
+router.post('/:orgId/users', authMiddleware, organisationController.addUserToOrganisation);
+
+module.exports = router;
