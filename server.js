d2064be8208d5c5172fa1b2198d3fd9eb9ab1979
ebaze-tech <139893207+ebaze-tech@users.noreply.github.com>
2024-07-06 00:04:02 +0100

    Update server.js

--- server.js
+++ server.js
@@ -25,3 +25,37 @@ sequelize.sync()
 
 
 module.exports = app;
+
+
+
+
+
+
+
+
+
+
+
+
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const authRoutes = require('./routes/authRoutes');
+const organisationRoutes = require('./routes/organisationRoutes');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use('/auth', authRoutes);
+app.use('/api/organisations', organisationRoutes);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
