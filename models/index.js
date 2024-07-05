5cc3492d8198d4e3b9a17eb45d8324e107b36f62
ebaze-tech <139893207+ebaze-tech@users.noreply.github.com>
2024-07-05 23:47:17 +0100

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,14 +1,39 @@
-const Sequelize = require('sequelize');
-const sequelize = require('../config/database');
+//const {Sequelize} = require('sequelize');
+//const sequelize = require('../config/database');
 
-const User = require('./user')(sequelize, Sequelize.DataTypes);
-const Organisation = require('./organisation')(sequelize, Sequelize.DataTypes);
+//const User = require('./user')(sequelize, Sequelize.DataTypes);
+//const Organisation = require('./organisation')(sequelize, Sequelize.DataTypes);
 
-User.belongsToMany(Organisation, { through: 'UserOrganisation' });
-Organisation.belongsToMany(User, {through: 'UserOrganisation'});
+//User.belongsToMany(Organisation, { through: 'UserOrganisation' });
+//Organisation.belongsToMany(User, {through: 'UserOrganisation'});
 
-module.exports = {
-    sequelize,
-    User,
-    Organisation
-};
+//module.exports = {
+//    sequelize,
+//    User,
+//    Organisation
+//};
+
+
+const { Sequelize } = require('sequelize');
+const config = require('../config/config');
+
+const sequelize = new Sequelize(
+  config.development.database,
+  config.development.username,
+  config.development.password,
+  {
+    host: config.development.host,
+    dialect: 'mysql',
+  }
+);
+
+const User = require('./user')(sequelize);
+const Organisation = require('./organisation')(sequelize);
+
+// Define relationships
+User.belongsToMany(Organisation, { through: 'UserOrganisations' });
+Organisation.belongsToMany(User, { through: 'UserOrganisations' });
+
+sequelize.sync();
+
+module.exports = { sequelize, User, Organisation };
