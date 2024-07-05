19b0248e4fbe44dd57285895c7ed46b9e3818794
ebaze-tech <139893207+ebaze-tech@users.noreply.github.com>
2024-07-05 23:39:47 +0100

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,9 +1,22 @@
+//require('dotenv').config();
+
+//module.exports = {
+//  DB_NAME: process.env.DB_NAME,
+//  DB_USER: process.env.DB_USER,
+//  DB_PASSWORD: process.env.DB_PASSWORD,
+//  DB_HOST: process.env.DB_HOST,
+//  DB_DIALECT: process.env.DB_DIALECT
+//};
+
+
 require('dotenv').config();
 
 module.exports = {
-  DB_NAME: process.env.DB_NAME,
-  DB_USER: process.env.DB_USER,
-  DB_PASSWORD: process.env.DB_PASSWORD,
-  DB_HOST: process.env.DB_HOST,
-  DB_DIALECT: process.env.DB_DIALECT
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+  },
 };
