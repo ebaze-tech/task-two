0b7b4688a340a9dba93fa9362e7191481d898197
ebaze-tech <139893207+ebaze-tech@users.noreply.github.com>
2024-07-05 23:42:36 +0100

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,32 +1,66 @@
-module.exports = (sequelize, DataTypes) => {
-    const User = sequelize.define('User', {
-      userId: {
-        type: DataTypes.STRING,
-        unique: true,
-        allowNull: false,
-      },
-      firstName: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      lastName: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      email: {
-        type: DataTypes.STRING,
-        unique: true,
-        allowNull: false,
-      },
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      phone: {
-        type: DataTypes.STRING,
-      },
-    });
+// module.exports = (sequelize, DataTypes) => {
+   // const User = sequelize.define('User', {
+    //  userId: {
+     //   type: DataTypes.STRING,
+     //   unique: true,
+    //    allowNull: false,
+     // },
+    //  firstName: {
+    //    type: DataTypes.STRING,
+    //    allowNull: false,
+    //  },
+    //  lastName: {
+     //   type: DataTypes.STRING,
+    //    allowNull: false,
+    //  },
+    //  email: {
+     //   type: DataTypes.STRING,
+     //   unique: true,
+       // allowNull: false,
+    //  },
+     // password: {
+      //  type: DataTypes.STRING,
+     //   allowNull: false,
+   //   },
+    //  phone: {
+       // type: DataTypes.STRING,
+     // },
+    //});
   
-    return User;
-  };
-  
+   // return User;
+//  };
+//}*
+
+const { DataTypes } = require('sequelize');
+const sequelize = require('./index');
+
+const User = sequelize.define('User', {
+  userId: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  lastName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  phone: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+});
+
+module.exports = User;
